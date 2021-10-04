7c237285c067171c9fb7f8d14b54ffcb97ae23ed
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-10-04 14:34:17 +0200

    Add exercise for CROSS JOINs

--- Ex 21_CROSS JOINs.sql
+++ Ex 21_CROSS JOINs.sql
@@ -12,3 +12,22 @@ SELECT
     p.name AS product
 FROM customers c, products p -- this is the equivalent to the explicit syntax's result
 ORDER BY c.first_name;
+
+-- Do a cross join between shippers and products
+-- using the implicit syntax
+-- and then using the explicit syntax
+
+-- implicit syntax of CROSS JOIN
+SELECT 
+	sh.name AS shippers,
+    p.name AS products
+FROM shippers sh, products p
+ORDER BY sh.name;
+
+-- explicit syntax of CROSS JOIN
+SELECT 
+	sh.name AS shippers,
+    p.name AS products
+FROM shippers sh
+CROSS JOIN products p
+ORDER BY sh.name;
