e475610ac9bb349a256309b32744535acae6dc78
Biljana Zobenica <32877624+biljana-zobenica@users.noreply.github.com>
2021-10-16 00:01:19 +0200

    Update Ex 40_Subqueries in the SELECT clause.sql

--- 05_Writing COMPLEX query/Ex 40_Subqueries in the SELECT clause.sql
+++ 05_Writing COMPLEX query/Ex 40_Subqueries in the SELECT clause.sql
@@ -1,23 +1,23 @@
 USE sql_invoicing;
 
 SELECT
-	invoice_id,
+    invoice_id,
     invoice_total,
     (SELECT AVG(invoice_total)
-		FROM invoices) AS invoice_average,
+	FROM invoices) AS invoice_average,
 	invoice_total - (SELECT invoice_average) AS difference
 FROM invoices;
 
 -- show all clients with thir total_sales for issued invoices
 
 SELECT 
-	client_id,
+    client_id,
     name,
     (SELECT SUM(invoice_total)
-		FROM invoices
+	FROM invoices
         WHERE client_id = c.client_id) AS total_sales,
     (SELECT AVG(invoice_total) 
-		FROM invoices) AS average, 
+	FROM invoices) AS average, 
 	(SELECT total_sales - average) AS difference
 FROM clients c;
 	
