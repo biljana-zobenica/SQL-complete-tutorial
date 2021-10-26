bcc4992dd761108220b0ac791ffea9bc3fd233fc
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-10-26 18:11:53 +0200

    Create view example

--- 07_Views/Ex 49_Creating views.sql
+++ 07_Views/Ex 49_Creating views.sql
@@ -1,5 +1,6 @@
 USE sql_invoicing;
 
+-- create a view
 CREATE VIEW sales_by_client AS
 SELECT 
 	c.client_id,
@@ -8,3 +9,35 @@ SELECT
 FROM clients c
 JOIN invoices i USING (client_id)
 GROUP BY client_id, name;
+
+-- use a view in SQL syntax
+SELECT * 
+FROM sales_by_client
+ORDER BY total_sales DESC;
+
+SELECT * 
+FROM sales_by_client
+WHERE total_sales > 500;
+
+SELECT * 
+FROM sales_by_client
+JOIN clients USING (client_id);
+
+-- Create a view to see the balance
+-- for each client.
+--
+-- clients_balance
+-- client_id
+-- name
+-- balance
+
+CREATE VIEW clients_balance AS
+SELECT 
+	c.client_id,
+	c.name,
+    SUM(invoice_total - payment_total) AS balance
+FROM clients c
+JOIN invoices i USING (client_id)
+GROUP BY client_id, name;
+
+SELECT * FROM clients_balance;
