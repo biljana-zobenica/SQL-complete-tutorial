f7596cd284616bcc7e1da129039afdcd6a9e06da
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-10-26 16:42:51 +0200

    Add Views example

---
+++ 07_Views/Ex 49_Creating views.sql
@@ -0,0 +1,10 @@
+USE sql_invoicing;
+
+CREATE VIEW sales_by_client AS
+SELECT 
+	c.client_id,
+    c.name,
+    SUM(invoice_total) AS total_sales
+FROM clients c
+JOIN invoices i USING (client_id)
+GROUP BY client_id, name;
