cb87f564bac1384e3e9ef7356bfba8ebd1f6b3cf
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-10-29 15:58:48 +0200

    CREATE stored procedure

---
+++ 08_Stored procedures/Ex 53_CREATE stored procedure.sql
@@ -0,0 +1,34 @@
+DELIMITER $$
+CREATE PROCEDURE get_clients()
+BEGIN
+	SELECT * FROM clients;
+END $$
+
+DELIMITER ;
+
+CALL get_clients();
+
+-- Create a stored procedure called
+-- get_invoices_with_balance
+-- to return all the invoices with a balance > 0
+
+DELIMITER $$
+CREATE PROCEDURE get_invoices_with_balance()
+BEGIN
+	SELECT * FROM invoices 
+	WHERE invoice_total - payment_total > 0;
+END $$
+DELIMITER ;
+
+CALL get_invoices_with_balance();.
+
+-- use the created view
+DELIMITER $$
+CREATE PROCEDURE get_invoices_with_balance_view()
+BEGIN
+	SELECT * FROM invoices_with_balance
+	WHERE balance > 0;
+END $$
+DELIMITER ;
+
+CALL get_invoices_with_balance_view();
