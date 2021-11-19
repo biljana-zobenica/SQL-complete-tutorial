c692ed2e8a544df84c25da0b6409120137b3ab73
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-11-19 20:11:01 +0100

    Stored procedure parameter exercies

--- 08_Stored procedures/Ex 56_ADD stored procedure parameter.sql
+++ 08_Stored procedures/Ex 56_ADD stored procedure parameter.sql
@@ -10,3 +10,21 @@ BEGIN
     WHERE c.state = state;
 END $$
 DELIMITER ;
+
+-- Write a stored procedure to return invoices
+-- for a given client
+-- 
+-- get_invoices_by_client
+
+DROP PROCEDURE IF EXISTS get_invoices_by_client;
+
+DELIMITER $$
+CREATE PROCEDURE get_invoices_by_client
+(
+	client_id INT
+)
+BEGIN
+	SELECT * FROM invoices i
+    WHERE i.client_id = client_id;
+END $$
+DELIMITER ;
