1109008a615ce3f95c9f36a360edcf810dcacc84
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-11-19 20:03:01 +0100

    Add stored procedure parameter

--- 08_Stored procedures/Ex 56_ADD stored procedure parameter.sql
+++ 08_Stored procedures/Ex 56_ADD stored procedure parameter.sql
@@ -1,8 +1,12 @@
 DROP PROCEDURE IF EXISTS get_clients_by_state;
 
 DELIMITER $$
-CREATE PROCEDURE get_clients_by_state(state(CHAR(2))
+CREATE PROCEDURE get_clients_by_state
+(
+	state CHAR(2)
+)
 BEGIN
-	SELECT * FROM clients;
+	SELECT * FROM clients c
+    WHERE c.state = state;
 END $$
 DELIMITER ;
