f29fa10aa1b022ade5aa9df21231d9328350eea9
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-11-19 21:07:37 +0100

    Stored procedure parameters with default value

--- 08_Stored procedures/Ex 56_ADD stored procedure parameter.sql
+++ 08_Stored procedures/Ex 56_ADD stored procedure parameter.sql
@@ -6,8 +6,17 @@ CREATE PROCEDURE get_clients_by_state
 	state CHAR(2)
 )
 BEGIN
-	SELECT * FROM clients c
-    WHERE c.state = state;
+	/* IF state IS NULL THEN
+		SELECT * FROM clients;
+	ELSE 
+		SELECT * FROM clients c
+		WHERE c.state = state;
+		-- SET state = 'CA';
+	END IF; */
+    -- cleaner code:
+    SELECT * FROM clients c
+    WHERE c.state = IFNULL(state, c.state);
+    
 END $$
 DELIMITER ;
 
@@ -26,5 +35,26 @@ CREATE PROCEDURE get_invoices_by_client
 BEGIN
 	SELECT * FROM invoices i
     WHERE i.client_id = client_id;
+END $$
+DELIMITER ;
+
+-- Write a stored procedure called get_payments
+-- with two parameters
+--
+-- client_id => INT
+-- payment_method_id => TINYINT
+
+DROP PROCEDURE IF EXISTS get_payments;
+
+DELIMITER $$
+CREATE PROCEDURE get_payments
+(
+	client_id INT,
+    payment_method_id TINYINT
+)
+BEGIN
+	SELECT * FROM payments p
+    WHERE p.client_id = IFNULL(client_id, p.client_id) AND
+		p.payment_method = IFNULL(payment_method_id, p.payment_method);
 END $$
 DELIMITER ;
