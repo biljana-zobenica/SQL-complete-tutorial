9015e5706ac5a16e4bdc697bafe781a9db606ebf
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-11-21 17:10:47 +0100

    Design a database_ER diagram

---
+++ 12_Designing databases/Ex 63_Designing a database_Steps.sql
@@ -0,0 +1,9 @@
+-- Designing a database:
+-- 1. understand the requirements
+-- 2. build a conceptual model
+-- 3. build a logical model
+-- 4. build a physical model
+
+-- CONCEPTUAL MODEL 	-> represents the entities and their relationships.
+-- LOGICAL MODEL 		-> creating relationships between entities; more details than the conceptual model.
+-- PHYSICAL MODEL		-> the implementation of logical model in mySQL.
