df179134e233ff26bd8599f97c21546387fa73cc
Biljana Zobenica <biljana.zobenica@outlook.com>
2021-11-24 17:23:09 +0100

    Isolation levels

--- 10_Transactions and Concurrency/Ex 70_CREATE a transaction.sql
+++ 10_Transactions and Concurrency/Ex 70_CREATE a transaction.sql
@@ -17,4 +17,14 @@ SHOW VARIABLES LIKE 'autocommit'; -- set ON by default;
 Database concurrency is the ability of a database to allow multiple 
 users to affect multiple transactions. This is one of the main properties
 that separates a database from other forms of data storage, like spreadsheets.
+
+Concurrency problems:
+1. lost updates
+2. dirty reads
+3. non-repeating reads
+4. phantom reads
 */
+
+SHOW VARIABLES LIKE 'transaction_isolation';
+SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE; -- only for the current session;
+SET GLOBAL TRANSACTION ISOLATION LEVEL SERIALIZABLE; -- set it globally;
