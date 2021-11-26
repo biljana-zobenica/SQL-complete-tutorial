73dd5a0c2cafbdc308c15e77bdb7b00762222c38
biljana-zobenica <biljana.zobenica@outlook.com>
2021-11-26 18:10:40 +0100

    View and drop user

--- 14_Securing databases/Ex 75_CREATE user account.sql
+++ 14_Securing databases/Ex 75_CREATE user account.sql
@@ -6,4 +6,9 @@ CREATE USER biljana@'%.biljana-zobenica.github.io' -- with any subdomains
 
 CREATE USER biljana IDENTIFIED BY '1234';
 
+-- View a list of user
+SELECT * FROM mysql.user;
 
+-- Dropping users
+CREATE USER bob@localhost IDENTIFIED BY '1234';
+DROP USER bob@localhost;
