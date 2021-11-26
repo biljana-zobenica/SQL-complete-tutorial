c98cb01c816fcd28420f40ecd62ebd76686b037b
biljana-zobenica <biljana.zobenica@outlook.com>
2021-11-26 18:37:34 +0100

    Grant and revoke privileges

--- 14_Securing databases/Ex 75_CREATE user account.sql
+++ 14_Securing databases/Ex 75_CREATE user account.sql
@@ -16,3 +16,29 @@ DROP USER bob@localhost;
 -- Change a user password
 SET PASSWORD FOR biljana = '1234'; -- or:
 SET PASSWORD = '1234'; -- set the password of currently logged user;
+
+-- Granting privileges
+-- 1: web/desktop application
+CREATE USER bika_app IDENTIFIED BY '1234';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE
+ON sql_store.* -- or specify a table name instead of all by *;
+TO bika_app;
+
+-- 2: admin
+GRANT ALL
+ON *.* -- (all tables in all databases)
+TO biljana;
+
+-- View privileges
+SHOW GRANTS FOR biljana;
+SHOW GRANTS; -- for current user
+
+-- Revoke privileges
+GRANT CREATE VIEW 
+ON sql_store.*
+TO bika_app;
+
+REVOKE CREATE VIEW 
+ON sql_store.*
+FROM bika_app;
