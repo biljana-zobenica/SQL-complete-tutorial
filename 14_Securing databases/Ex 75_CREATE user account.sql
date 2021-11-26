98c32c61ec25acd39d08f21ee885054e5f8142bc
biljana-zobenica <biljana.zobenica@outlook.com>
2021-11-26 18:06:36 +0100

    Create user account

---
+++ 14_Securing databases/Ex 75_CREATE user account.sql
@@ -0,0 +1,9 @@
+-- Create a user account
+CREATE USER biljana@127.0.0.1 -- with IP
+CREATE USER biljana@localhost -- with host
+CREATE USER biljana@biljana-zobenica.github.io -- with domain
+CREATE USER biljana@'%.biljana-zobenica.github.io' -- with any subdomains
+
+CREATE USER biljana IDENTIFIED BY '1234';
+
+
