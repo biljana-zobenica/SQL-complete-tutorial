62c0f1ada2c4824cc421d6bd265af44db3fa782f
biljana-zobenica <biljana.zobenica@outlook.com>
2021-11-29 18:40:08 +0100

    Add Data Wrangling

---
+++ 15_Additional_SQL_MODE.com/2_Advanced SQL/22_Data Wrangling.sql
@@ -0,0 +1,6 @@
+/* Data wrangling (or munging) is the process of programmatically
+ transforming data into a format that makes it easier to work with. */
+ 
+ -- Using SQL String Functions to Clean Data
+ 
+ 
