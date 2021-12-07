e7017a8eb4ecda8393ef781b512e4d8c15c87b5d
biljana-zobenica <biljana.zobenica@outlook.com>
2021-12-07 17:19:45 +0100

    Create and drop database, create and alter table

--- 12_Designing databases/Ex 63_Designing a database_Steps.sql
+++ 12_Designing databases/Ex 63_Designing a database_Steps.sql
@@ -6,4 +6,36 @@
 
 -- CONCEPTUAL MODEL 	-> represents the entities and their relationships.
 -- LOGICAL MODEL 		-> creating relationships between entities; more details than the conceptual model.
--- PHYSICAL MODEL		-> the implementation of logical model in mySQL.courses
+-- PHYSICAL MODEL		-> the implementation of logical model in mySQL.courses 
+
+
+-- create a databse
+
+CREATE DATABASE IF NOT EXISTS sql_store2;
+DROP DATABASE IF EXISTS sql_store2;
+
+-- create a table
+
+USE sql_store2;
+DROP TABLE IF EXISTS customers; -- or CREATE TABLE IF NOT EXISTS customers
+CREATE TABLE customers
+	(
+		customer_id 	INT PRIMARY KEY AUTO_INCREMENT,
+		first_name 		VARCHAR(50) NOT NULL,
+        points			INT NOT NULL DEFAULT 0,
+        email			VARCHAR(255) NOT NULL UNIQUE
+	);
+    
+-- alter a table
+
+ALTER TABLE customers
+	ADD last_name 	VARCHAR(50) NOT NULL AFTER first_name,
+    ADD city 		VARCHAR(50) NOT NULL,
+    MODIFY COLUMN first_name VARCHAR(55) DEFAULT '',
+    DROP points;
+    
+    
+    
+    
+    
+    
