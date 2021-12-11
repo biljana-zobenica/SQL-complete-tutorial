bed6820009dd75532d8051e7ba1e78e85c6e6d53
biljana-zobenica <biljana.zobenica@outlook.com>
2021-12-11 22:38:43 +0100

    Problem description

---
+++ 15_Additional_SQL_MODE.com/3_SQL Analytics Training/30_Analytics cases_Yammer.sql
@@ -0,0 +1,14 @@
+/* Analytics case: Yammer
+
+Yammer is a social network for communicating with coworkers. Individuals share
+documents, updates, and ideas by posting them in groups. Yammer is free to use
+indefinitely, but companies must pay license fees if they want access to
+administrative controls, including integration with user management systems 
+like ActiveDirectory.
+
+Yammer analysts are trained to constantly consider the value of each individual 
+project; they seek to maximize the return on their time. Analysts typically opt 
+for less precise solutions to problems if it means investing substantially less 
+time as well. */
+
+
